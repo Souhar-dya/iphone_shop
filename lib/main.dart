e30937ec4ab58f11efe8a8451680bacccad19cbf
Souhar-dya <kundusouhardya@gmail.com>
2023-03-10 22:19:52 +0530

    Creating the login page and adding some google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:iphone_shop/pages/login_page.dart';
 import 'pages/home_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +10,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       /* home: const HomePage() */
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.acme().fontFamily,
+      ),
       routes: {
         "/": (context) => LoginPage(),
         "/home": (context) => const HomePage(),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -4,17 +4,50 @@ class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          "Login Page",
-          style: TextStyle(
-            fontWeight: FontWeight.w700,
-            color: Colors.deepPurple,
-            fontSize: 30,
+    return Material(
+      color: Colors.white,
+      child: Column(children: [
+        Image.asset(
+          "assets/images/login_image.png",
+          fit: BoxFit.cover,
+        ),
+        const SizedBox(
+          height: 10.0,
+        ),
+        const Text(
+          "Welcome",
+          style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 55.0),
+          child: Column(
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                    hintText: "Enter username", labelText: "Username"),
+              ),
+              TextFormField(
+                obscureText: true,
+                decoration: const InputDecoration(
+                    hintText: "Enter password", labelText: "Password"),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  print("HI Souhardya");
+                },
+                style: const ButtonStyle(
+                    backgroundColor:
+                        MaterialStatePropertyAll(Colors.deepPurple),
+                    foregroundColor: MaterialStatePropertyAll(Colors.white)),
+                child: const Text("Login"),
+              )
+            ],
           ),
         ),
-      ),
+      ]),
     );
   }
 }
