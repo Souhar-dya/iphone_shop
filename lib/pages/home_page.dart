d7a6ecfd649434f1a4b718fd5e0f9dd0863fef87
Souhar-dya <kundusouhardya@gmail.com>
2023-03-12 11:28:51 +0530

    Attaching from json files

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,25 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import '../models/catalog.dart';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
-  final dummyList = List.generate(4, (index) => CatalogModel.items[0]);
+
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    await Future.delayed(Duration(seconds: 2));
+    var catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+    var decodedData = jsonDecode(catalogJson);
+    var productsData = decodedData["products"];
+    CatalogModel.items = List.from(productsData)
+        .map<Item>((item) => Item.fromMap(item))
+        .toList();
+    setState(() {});
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: const Text("Catalog App"),
       ),
-      body: ListView.builder(
-        padding: EdgeInsets.all(16),
-        itemCount: dummyList.length,
-        itemBuilder: (context, index) {
-          return ItemWidget(dummyList[index]);
-        },
-      ),
-      drawer: MyDrawer(),
+      body: (CatalogModel.items != null) && (CatalogModel.items.isNotEmpty)
+          ? ListView.builder(
+              padding: const EdgeInsets.all(12),
+              itemCount: CatalogModel.items.length,
+              itemBuilder: (context, index) {
+                return ItemWidget(CatalogModel.items[index]);
+              },
+            )
+          : const Center(
+              child: CircularProgressIndicator(),
+            ),
+      drawer: const MyDrawer(),
     );
   }
 }
